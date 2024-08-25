914ded51af90f370c4351d19862bbc3eafe6b076
MinhKhanh614 <0308231027@caothang.edu.vn>
2024-08-25 20:56:19 +0700

    thêm test Serial và test snippet thành công

---
+++ src/khanhTest_KxnTask_Snippet.c
@@ -0,0 +1,110 @@
+#pragma once
+
+#include "kxnTask.h"
+
+DEFINE_TASK_STATE(khanhTest_KxnTask_Snippet){
+    khanhTest_KxnTask_Snippet_ON,
+    khanhTest_KxnTask_Snippet_OFF,
+};
+
+CREATE_TASK(khanhTest_KxnTask_Snippet)
+/*Add your variable here*/
+
+void setup()
+{
+    /*Add your code setup here*/
+    stop();
+}
+
+void loop()
+{
+    switch (getState())
+    {
+    case khanhTest_KxnTask_Snippet_ON:
+        /*code*/
+        kDelay(0);
+        setState(khanhTest_KxnTask_Snippet_OFF);
+        break;
+
+    case khanhTest_KxnTask_Snippet_OFF:
+        /*code*/
+        kDelay(0);
+        setState(khanhTest_KxnTask_Snippet_ON);
+        break;
+
+    default:
+        break;
+    }
+}
+
+void start()
+{
+    kxnTaskManager.add(this);
+    setState(khanhTest_KxnTask_Snippet_ON);
+}
+
+void stop()
+{
+    kDelay(0);
+    setStateIdle();
+}
+
+END
+
+#ifndef khanhTest_KxnTask_Snippet__H
+#define khanhTest_KxnTask_Snippet__H
+
+#include "kxnTask.h"
+
+DEFINE_TASK_STATE(khanhTest_KxnTask_Snippet){
+    khanhTest_KxnTask_Snippet_ON,
+    khanhTest_KxnTask_Snippet_OFF,
+};
+
+CREATE_TASK(khanhTest_KxnTask_Snippet)
+/*Add your variable here*/
+
+void setup()
+{
+    /*Add your code setup here*/
+    stop();
+}
+
+void loop()
+{
+    switch (getState())
+    {
+    case khanhTest_KxnTask_Snippet_ON:
+        /*code*/
+        kDelay(0);
+        setState(khanhTest_KxnTask_Snippet_OFF);
+        break;
+
+    case khanhTest_KxnTask_Snippet_OFF:
+        /*code*/
+        kDelay(0);
+        setState(khanhTest_KxnTask_Snippet_ON);
+        break;
+
+    default:
+        break;
+    }
+}
+
+void start()
+{
+    kxnTaskManager.add(this);
+    setState(khanhTest_KxnTask_Snippet_ON);
+}
+
+void stop()
+{
+    kDelay(0);
+    setStateIdle();
+}
+
+void runFast(){
+
+}
+
+END
--- src/test_lib_1.h
+++ src/test_lib_1.h
@@ -1,4 +1,25 @@
-#ifndef kxnTaskLED_h
+// Tiền xử lý
+/**
+ * Bảo vệ Bao gồm Truyền Thống
+ * 
+ * #ifndef EXAMPLE_H: Đây là viết tắt của “if not defined”. 
+ * Nó kiểm tra xem EXAMPLE_H đã được định nghĩa trước đó hay chưa.
+ * 
+ * #define EXAMPLE_H: Nếu EXAMPLE_H chưa được định nghĩa, dòng này sẽ định nghĩa nó.
+ * Điều này đảm bảo rằng lần sau khi tệp tiêu đề này được bao gồm, 
+ * EXAMPLE_H đã được định nghĩa và đoạn mã giữa #ifndef và #endif sẽ bị bỏ qua.
+ * 
+ * Kết thúc Bảo vệ Bao gồm
+ * #endif // EXAMPLE_H
+ * #endif: Kết thúc khối điều kiện bắt đầu bằng #ifndef. 
+ * Nếu EXAMPLE_H chưa được định nghĩa, đoạn mã giữa #ifndef và #endif sẽ được biên dịch. 
+ * Nếu không, đoạn mã này sẽ bị bỏ qua.
+ * 
+ * #pragma once là một chỉ thị tiền xử lý trong C và C++ được sử dụng để đảm bảo rằng một tệp tiêu đề chỉ được bao gồm một lần trong quá trình biên dịch. 
+ * Điều này giúp tránh các lỗi do bao gồm lặp lại tệp tiêu đề, chẳng hạn như định nghĩa lại các biến hoặc hàm.
+ * 
+ */
+#ifndef kxnTaskLED_h    
 #define kxnTaskLED_h
 #pragma once
 #include "kxnTask.h"
